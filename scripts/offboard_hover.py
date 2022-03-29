00b573def0b2935c7bb0ec3821aa27a137e99285
Troi Williams <40696868+troiwill@users.noreply.github.com>
2022-03-29 13:08:18 -0400

    Added switch to hover mode at end.

--- scripts/offboard_hover.py
+++ scripts/offboard_hover.py
@@ -2,7 +2,7 @@
 
 import rospy
 
-from mavros_px4_vehicle.px4_modes import PX4_MODE_OFFBOARD
+from mavros_px4_vehicle.px4_modes import PX4_MODE_LOITER, PX4_MODE_OFFBOARD
 from mavros_px4_vehicle.px4_offboard_modes import SetPositionCmdBuilder
 from mavros_px4_vehicle.px4_vehicle import PX4Vehicle
 
@@ -22,6 +22,10 @@ def offboard_hover():
 
     rospy.loginfo("Changing to offboard mode.")
     copter.set_mode(PX4_MODE_OFFBOARD)
+    rospy.sleep(10.)
+
+    rospy.loginfo("Changing to hover mode.")
+    copter.set_mode(PX4_MODE_LOITER)
     rospy.spin()
 #end def
 
